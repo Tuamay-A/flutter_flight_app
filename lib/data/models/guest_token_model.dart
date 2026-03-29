44659cf3f54202216b7e68e9fa12e01f8299d11b
Tuamay <tuamayassefa@gmail.com>
2026-03-29 04:55:43 -0700

    updeted guest token

--- lib/data/models/guest_token_model.dart
+++ lib/data/models/guest_token_model.dart
@@ -1,17 +1,31 @@
 class GuestTokenResponse {
   final String guestToken;
+  final String agencyType;
+  final int expiresIn;
+  final String message;
 
-  GuestTokenResponse({required this.guestToken});
+  GuestTokenResponse({
+    required this.guestToken,
+    this.agencyType = '',
+    this.expiresIn = 3600,
+    this.message = '',
+  });
 
   factory GuestTokenResponse.fromJson(Map<String, dynamic> json) {
     return GuestTokenResponse(
       guestToken: json['guestToken'] ?? json['guest_token'] ?? '',
+      agencyType: json['agencyType'] ?? '',
+      expiresIn: json['expiresIn'] ?? 3600,
+      message: json['message'] ?? '',
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'guestToken': guestToken,
+      'agencyType': agencyType,
+      'expiresIn': expiresIn,
+      'message': message,
     };
   }
 }
